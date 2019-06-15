dd7a878b781930393040350365fc8d36a34c4e3b
TheCooler <truecooler@gmail.com>
2019-06-15 06:03:44 +0300

    table users fix

--- FridayClean.Server/DataBaseModels/User.cs
+++ FridayClean.Server/DataBaseModels/User.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace FridayClean.Server.DataBaseModels
 {
-
+	[Table(name:"Users")]
 	public class User
 	{
+		[Key]
 		public int Id;
 
 		public string Phone;
